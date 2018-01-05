dc50a1972e9c06204aac0c5f307cd17417b54a17
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2018-01-05 16:14:12 -0600

    Sube fotos de galería y cámara

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Upload Images Ionic 3
+Este proyecto es un ejemplo para subir imágenes desde la cámara y galería con
+ Ionic 3 a un servidor php con Laravel.
+ 
+## Instalación automática de dependencias
+```
+npm install
+ionic serve
+ionic state restore
+```
+
+
+## Instalación manual de dependencias
+```
+npm install --save @ionic-native/camera 
+npm install --save @ionic-native/file
+npm install --save @ionic-native/file-path
+npm install --save @ionic-native/file-transfer
+ionic cordova plugin add cordova-plugin-camera --save
+ionic cordova plugin add cordova-plugin-file --save
+ionic cordova plugin add cordova-plugin-file-transfer --save
+ionic cordova plugin add cordova-plugin-filepath --save
+```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+
+import {File} from '@ionic-native/file';
+import {FileTransfer} from '@ionic-native/file-transfer';
+import {FilePath} from '@ionic-native/file-path';
+import {Camera} from '@ionic-native/camera';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        File,
+        FileTransfer,
+        FilePath,
+        Camera,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
